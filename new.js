235cf77f87f53bd1f34ccc3c159c38578c198e8f
hadeelamr <82241787+hadeelamr@users.noreply.github.com>
2021-10-16 23:53:14 +0300

    Create new.js

---
+++ new.js
@@ -0,0 +1,13 @@
+let array=['dog','cat','lion','tiger'];
+for(let i=0;i<array.length;i++){
+    console.log(array[i]);
+}
+function multiple(x,y){
+    var result=x*y;
+    console.log(result);
+}
+multiple(2,4);
+
+let name='hadeel';
+console.log(name.length);
+console.log(name.replace('el','26'));
